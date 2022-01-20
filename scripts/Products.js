1643996c58542d218f22bb3b43cc9f3ae98339c8
Kevin Caruso <Kjcaruso12@gmail.com>
2022-01-20 14:33:40 -0600

    Added event listener to products and changed the output to have an a floating number of 2

--- scripts/Products.js
+++ scripts/Products.js
@@ -1,5 +1,22 @@
 import { getProducts } from "./database.js"
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("product")) {
+            const [,productId] = itemClicked.id.split("--")
+
+            for (const product of products) {
+                if (product.id === parseFloat(productId)) {
+                    product.price = parseFloat(product.price).toFixed(2)
+                    window.alert(`${product.name} costs\n $${product.price}`)
+                }
+            }
+        }
+    }
+)
+
 const products = getProducts()
 
 export const Products = () => {
